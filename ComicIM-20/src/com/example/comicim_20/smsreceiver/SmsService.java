773a8c68b8eca72d444c4105a8ee78b49d05e091
Edman274 <dudeman_site@hotmail.com>
2014-10-04 19:07:52 -0400

    Send inter activity messages to update the MessageView when we get a new SMS.

---
+++ ComicIM-20/src/com/example/comicim_20/smsreceiver/SmsService.java
@@ -0,0 +1,15 @@
+package com.example.comicim_20.smsreceiver;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SmsService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
