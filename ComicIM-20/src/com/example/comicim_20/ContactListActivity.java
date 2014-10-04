82728270a159459cc8d2b46e799ed43e77c5a456
Edman274 <dudeman_site@hotmail.com>
2014-10-04 12:33:13 -0400

    Add contact / phone number handling code.

--- ComicIM-20/src/com/example/comicim_20/ContactListActivity.java
+++ ComicIM-20/src/com/example/comicim_20/ContactListActivity.java
@@ -6,9 +6,16 @@ import java.util.List;
 import com.example.comicim_20.contactlist.ContactDatabaseHelper;
 import com.example.comicim_20.contactlist.ContactListAdapter;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,12 +31,15 @@ public class ContactListActivity extends ActionBarActivity {
 	public ContactDatabaseHelper databaseHelper;
 	public List<Contact> contacts = new ArrayList<Contact>();
 	public ListView contactListView;
+	private final int PICK_CONTACT = 1;
+	
 	
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_list_activity);
+        
         
         contacts.add(new Contact("+79037852844"));
         contacts.add(new Contact("+16147079195"));
@@ -44,6 +54,7 @@ public class ContactListActivity extends ActionBarActivity {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 				Log.i(TAG, "Clicked on " + contacts.get(position).phoneNumber);
+				
 			}
         });
     }
@@ -62,9 +73,32 @@ public class ContactListActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        //if (id == R.id.action_settings) {
-        //    return true;
-        //}
+        if (id == R.id.action_add_conversation){
+        	Intent contactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+			contactIntent.setType(Phone.CONTENT_TYPE); // This line is important! 
+			startActivityForResult(contactIntent, PICK_CONTACT);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+    
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+			switch(requestCode) {
+			case(PICK_CONTACT):
+				if(resultCode == Activity.RESULT_OK) {
+					Uri contactUri = data.getData();
+					String[] projection = {Phone.NUMBER};
+					Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
+					cursor.moveToFirst();
+					int column = cursor.getColumnIndex(Phone.NUMBER);
+					
+					//
+					String phoneNumber = PhoneNumberUtils.stripSeparators(cursor.getString(column));
+					Log.d(TAG, phoneNumber);
+					//recipientTextView.setText(phoneNumber);
+					//updateConversationView(phoneNumber, frameView, textView1,(LinearLayout)findViewById(R.id.linearLayout1));
+				}
+			}
+	}
 }
