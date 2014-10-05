b549c8a12398a6680247b4ffd8874762752e1f4b
lpatino10 <loganpatino10@gmail.com>
2014-10-05 03:05:04 -0400

    Added name to contact list view, changed header of messageView.java by adding contact name, removing number.

--- ComicIM-20/src/com/comicim/Conversation.java
+++ ComicIM-20/src/com/comicim/Conversation.java
@@ -4,6 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class Conversation {
+	public Conversation(long id, String phoneNumber, String name) {
+		this.id = id;
+		this.phoneNumber = phoneNumber;
+		this.name = name;
+		this.messages = new ArrayList<Message>();
+	}
+	
 	public Conversation(long id, String phoneNumber) {
 		this.id = id;
 		this.phoneNumber = phoneNumber;
@@ -12,6 +19,6 @@ public final class Conversation {
 
 	public long id;
 	public String phoneNumber;
-
+	public String name;
 	public List<Message> messages;
 }
--- ComicIM-20/src/com/comicim/messageview/MessageView.java
+++ ComicIM-20/src/com/comicim/messageview/MessageView.java
@@ -39,7 +39,6 @@ import android.content.IntentFilter;
 
 public class MessageView extends Activity implements ConversationListener {
 	private static final String TAG = "MessageView";
-	
 	private CoreService service;
 	private final ServiceConnection serviceConnection = new ServiceConnection() {
 		@Override
@@ -54,11 +53,12 @@ public class MessageView extends Activity implements ConversationListener {
 		}
 	};
 	protected void onServiceConnected() {
-		Log.i(TAG, "Service connected.");
+		Log.i(TAG, "Service connected.");		
 		
 		service.addListener(this);
 		conversation = service.getConversation(conversationId);
-		contactInfo.setText(conversation.phoneNumber);
+		setTitle(conversation.name);
+		//contactInfo.setText("Phone: " + conversation.phoneNumber);
 		
         messageAdapter = new MessageAdapter(this, this.conversation);
         list.setAdapter(messageAdapter);
@@ -110,7 +110,7 @@ public class MessageView extends Activity implements ConversationListener {
         this.startService(new Intent(this, CoreService.class));
 		this.bindService(new Intent(this, CoreService.class), this.serviceConnection, Context.BIND_AUTO_CREATE);
         
-		contactInfo = (TextView)findViewById(R.id.contactInf);
+		//contactInfo = (TextView)findViewById(R.id.contactInf);
 		enterText = (TextView)findViewById(R.id.messageField);
 		sendButton = (Button)findViewById(R.id.buttonSend);
         charCount = (TextView) findViewById(R.id.charCount);
